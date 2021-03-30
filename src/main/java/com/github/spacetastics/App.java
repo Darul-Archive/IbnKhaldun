8ddcaa9455c2787d8463d979ecb6a67da57263a8
DanielMc <daniel_mckerrocher@ultimatesoftware.com>
2021-03-30 13:11:35 -0400

    Initial code scaffolding

---
+++ src/main/java/com/github/spacetastics/App.java
@@ -0,0 +1,7 @@
+package com.github.spacetastics;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bot!");
+    }
+}
---
+++ src/test/java/com/gthub/spacetastics/AppTests.java
@@ -0,0 +1,12 @@
+package com.gthub.spacetastics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTests {
+    @Test
+    public void mainTest() {
+        assertTrue(true);
+    }
+}
